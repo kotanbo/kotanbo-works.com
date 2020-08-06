394a5bb1940087548c7e0e11f26cfaff38d2bed8
kotanbo <kotanbo@users.noreply.github.com>
2020-08-06 14:13:57 +0000

    deploy: e32bf2598a1ca78cab3395cff56ca5b823c39ce2

---
+++ component---src-pages-404-tsx-38ca4919b2112557039a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{"i6+/":function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),u=n.n(a),i=n("9Dj+");t.default=function(){return u.a.createElement(i.a,null,u.a.createElement("div",{id:"main"},u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness.")))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-38ca4919b2112557039a.js.map
