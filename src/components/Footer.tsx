5eb83ad359a56f9a2ece6b8a55ee3573e3ab7872
kota okamoto <kota_okamoto@kotanbo-works.com>
2020-08-06 18:17:13 +0900

    Share GraphQL for siteMetadata

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,38 +1,36 @@
 import React from 'react'
+import { useSiteMetadata } from '../hooks'
 
-type Props = {
-  user: {
-    name: string
-    mail: string
-    github: string
-  }
-}
+const Footer: React.FC = () => {
+  const siteMetadata = useSiteMetadata()
+  const user = siteMetadata.user
 
-const Footer: React.FC<Props> = ({ user }) => (
-  <div id="footer">
-    <div className="inner">
-      <ul className="icons">
-        <li>
-          <a href={`https://github.com/${user.github}`} className="icon fa-github">
-            <span className="label">GitHub</span>
-          </a>
-        </li>
-        <li>
-          <a href={`mailto:${user.mail}`} className="icon fa-envelope-o">
-            <span className="label">Email</span>
-          </a>
-        </li>
-      </ul>
-      <ul className="copyright">
-        <li>
-          &copy; {new Date().getFullYear()} {user.name}
-        </li>
-        <li>
-          Design: <a href="http://html5up.net">HTML5 UP</a>
-        </li>
-      </ul>
+  return (
+    <div id="footer">
+      <div className="inner">
+        <ul className="icons">
+          <li>
+            <a href={`https://github.com/${user.github}`} className="icon fa-github">
+              <span className="label">GitHub</span>
+            </a>
+          </li>
+          <li>
+            <a href={`mailto:${user.mail}`} className="icon fa-envelope-o">
+              <span className="label">Email</span>
+            </a>
+          </li>
+        </ul>
+        <ul className="copyright">
+          <li>
+            &copy; {new Date().getFullYear()} {user.name}
+          </li>
+          <li>
+            Design: <a href="http://html5up.net">HTML5 UP</a>
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Footer
