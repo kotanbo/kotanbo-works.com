7c5b199bfe1305d6fcfd838932e558a618908a7c
kotanbo <kota_okamoto@kotanbo-works.com>
2023-10-25 19:03:37 +0900

    Resolved error caused by URL prefix 'webpack:///mini-css-extract-plugin/'

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   output: 'export',
   images: {
     unoptimized: true
+  },
+  // URL 接頭辞 "webpack:///mini-css-extract-plugin/" によるエラー回避
+  // https://github.com/webpack-contrib/mini-css-extract-plugin/issues/945
+  webpack: (config) => {
+    plugins: [
+      (config.module.generator.asset.publicPath = '/_next/'),
+      new MiniCssExtractPlugin({
+        experimentalUseImportModule: false
+      })
+    ];
+    return config;
   }
 };
